8f0e26c0fd27f55a75079c50f88db5b6bc8ceca5
Dan Jacob <danjac2018@gmail.com>
2020-12-29 19:01:20 +0200

    Update setup.py

---
+++ build/lib/turbo_response/renderers.py
@@ -0,0 +1,103 @@
+# Standard Library
+import enum
+
+# Django
+from django.template.loader import render_to_string
+
+
+class Action(enum.Enum):
+    """Turbo-Stream action parameter"""
+
+    APPEND = "append"
+    PREPEND = "prepend"
+    REMOVE = "remove"
+    REPLACE = "replace"
+    UPDATE = "update"
+
+
+def render_turbo_stream(action, target, content=""):
+    """Wraps content in correct <turbo-stream> tags.
+
+    :param action: action type
+    :param target: the DOM ID target of the stream
+    :param content: content to be wrapped. Can be empty.
+
+    :type action: turbo_response.Action
+    :type target: str
+    :type content: str
+
+    :return: *<turbo-stream>* string
+    :rtype: str
+
+    """
+    return f'<turbo-stream action="{action.value}" target="{target}"><template>{content.strip()}</template></turbo-stream>'
+
+
+def render_turbo_frame(dom_id, content=""):
+    """
+
+    Wraps a response in correct *<turbo-frame>* tags.
+
+    :param dom_id: a DOM ID present in the content
+    :param content: content of the turbo-frame
+    :type dom_id: str
+    :type content: str
+
+    :rtype: str
+    """
+    return f'<turbo-frame id="{dom_id}">{content.strip()}</turbo-frame>'
+
+
+def render_turbo_stream_template(
+    template, context, *, action, target, **template_kwargs
+):
+    """Renders a *<turbo-stream>* template.
+
+    :param template: template name or names
+    :param context: template context
+    :param action: turbo-stream action
+    :param target: turbo-stream target
+    :type template: list or str
+    :type context: dict
+    :type action: turbo_response.Action
+    :type target: str
+
+    :rtype: str
+    """
+    return render_turbo_stream(
+        action,
+        target,
+        render_to_string(
+            template,
+            {
+                **context,
+                "turbo_stream_target": target,
+                "turbo_stream_action": action.value,
+                "is_turbo_stream": True,
+            },
+            **template_kwargs,
+        ),
+    )
+
+
+def render_turbo_frame_template(template, context, *, dom_id, **kwargs):
+    """Renders a *<turbo-frame>* template.
+
+    :param template: template name or names
+    :param context: template context
+    :param dom: turbo-frame DOM ID
+    :type template: list or str
+    :type context: dict
+    :type dom_id: str
+
+    :rtype: str
+    """
+
+    return render_turbo_frame(
+        dom_id,
+        render_to_string(
+            template,
+            {**context, "turbo_frame_dom_id": dom_id, "is_turbo_frame": True},
+            **kwargs,
+        ),
+    )
---
+++ build/lib/turbo_response/response.py
@@ -0,0 +1,103 @@
+# Django
+from django.http import HttpResponse, StreamingHttpResponse
+from django.template.response import TemplateResponse
+
+# Local
+from .renderers import render_turbo_frame, render_turbo_stream
+
+
+class TurboStreamResponseMixin:
+    """Automatically sets the correct turbo-stream content type."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            content_type="text/html; turbo-stream; charset=utf-8", *args, **kwargs
+        )
+
+
+class TurboStreamStreamingResponse(TurboStreamResponseMixin, StreamingHttpResponse):
+    """Handles turbo-stream responses. Generator should yield individual
+    turbo-stream strings."""
+
+
+class TurboStreamResponse(TurboStreamResponseMixin, HttpResponse):
+    """Basic turbo-stream response."""
+
+    def __init__(self, content=b"", *, action, target, **kwargs):
+        super().__init__(
+            render_turbo_stream(action, target, content), **kwargs,
+        )
+
+
+class TurboStreamTemplateResponse(TurboStreamResponseMixin, TemplateResponse):
+    """Handles turbo-stream template response.
+
+    Adds the following variables to the template:
+
+    - **is_turbo_stream**
+    - **turbo_stream_action**
+    - **turbo_stream_target**
+
+    """
+
+    is_turbo_stream = True
+
+    def __init__(self, request, template, context, *, action, target, **kwargs):
+
+        super().__init__(
+            request,
+            template,
+            {
+                **context,
+                "turbo_stream_action": action.value,
+                "turbo_stream_target": target,
+                "is_turbo_stream": True,
+            },
+            **kwargs,
+        )
+
+        self._target = target
+        self._action = action
+
+    @property
+    def rendered_content(self):
+        return render_turbo_stream(
+            action=self._action, target=self._target, content=super().rendered_content
+        )
+
+
+class TurboFrameResponse(HttpResponse):
+    """Handles turbo-frame template response."""
+
+    def __init__(self, content=b"", *, dom_id, **kwargs):
+        super().__init__(
+            render_turbo_frame(dom_id, content), **kwargs,
+        )
+
+
+class TurboFrameTemplateResponse(TemplateResponse):
+    """Handles turbo-stream template response.
+
+    Adds the following variables to the template:
+
+    - **is_turbo_frame**
+    - **turbo_frame_dom_id**
+
+    """
+
+    is_turbo_frame = True
+
+    def __init__(self, request, template, context, *, dom_id, **kwargs):
+
+        super().__init__(
+            request,
+            template,
+            {**context, "turbo_frame_dom_id": dom_id, "is_turbo_frame": True},
+            **kwargs,
+        )
+
+        self._dom_id = dom_id
+
+    @property
+    def rendered_content(self):
+        return render_turbo_frame(self._dom_id, super().rendered_content)
