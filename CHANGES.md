801659324ea44d6ce0c805db7fc67ff8f8890753
Dan Jacob <danjac2018@gmail.com>
2020-12-30 14:09:54 +0200

    Version bump

---
+++ CHANGES.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [0.0.8] - 2020-12-30
+
+### Added
+
+TurboStream and TurboFrame classes
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # Third Party Libraries
 from setuptools import setup
 
-version = "0.0.7"
+version = "0.0.8"
 
 setup(
     name="django-turbo-response",
