1e8315631682ed3d6a305f6c9b781767ed1484d7
Dan Jacob <danjac2018@gmail.com>
2021-04-01 20:58:43 +0300

    More tests for stream methods

--- src/turbo_response/tests/test_stream.py
+++ src/turbo_response/tests/test_stream.py
@@ -10,6 +10,20 @@ class TestTurboStream:
             == '<turbo-stream action="append" target="my-form"><template>OK</template></turbo-stream>'
         )
 
+    def test_render_xss(self):
+        s = TurboStream("my-form").append.render("<script></script>")
+        assert (
+            s
+            == '<turbo-stream action="append" target="my-form"><template>&lt;script&gt;&lt;/script&gt;</template></turbo-stream>'
+        )
+
+    def test_render_is_safe(self):
+        s = TurboStream("my-form").append.render("<script></script>", is_safe=True)
+        assert (
+            s
+            == '<turbo-stream action="append" target="my-form"><template><script></script></template></turbo-stream>'
+        )
+
     def test_template(self):
         s = (
             TurboStream("my-form")
@@ -17,6 +31,15 @@ class TestTurboStream:
             .render()
         )
         assert "my content" in s
+        assert '<turbo-stream action="append" target="my-form">' in s
+
+    def test_template_xss(self):
+        s = (
+            TurboStream("my-form")
+            .append.template("simple.html", {"msg": "<script></script>"})
+            .render()
+        )
+        assert "&lt;script&gt;&lt;/script&gt;" in s
         assert '<turbo-stream action="append" target="my-form">' in s
 
     def test_template_with_req_init(self, rf):
@@ -41,6 +64,18 @@ class TestTurboStream:
         resp = TurboStream("my-form").append.response("OK")
         assert resp.status_code == 200
         assert b"OK" in resp.content
+        assert b'<turbo-stream action="append" target="my-form"' in resp.content
+
+    def test_response_xss(self):
+        resp = TurboStream("my-form").append.response("<script></script>")
+        assert resp.status_code == 200
+        assert b"&lt;script&gt;&lt;/script&gt;" in resp.content
+        assert b'<turbo-stream action="append" target="my-form"' in resp.content
+
+    def test_response_is_safe(self):
+        resp = TurboStream("my-form").append.response("<script></script>", is_safe=True)
+        assert resp.status_code == 200
+        assert b"<script></script>" in resp.content
         assert b'<turbo-stream action="append" target="my-form"' in resp.content
 
     def test_template_response_req_init(self, rf):
