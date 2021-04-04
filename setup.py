0052faee1fa918e9ed8ad67faaee8c0efb8b97ad
Dan Jacob <danjac2018@gmail.com>
2021-04-04 12:40:12 +0300

    Add 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
