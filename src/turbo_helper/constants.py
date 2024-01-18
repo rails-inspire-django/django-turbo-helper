3c95809df8c0e0b0ff59e7107e0519ddaf773c18
Michaelyin <michaelyin@accordbox.com>
2024-01-18 16:24:29 +0800

    update ResponseFormat

--- src/turbo_helper/constants.py
+++ src/turbo_helper/constants.py
@@ -1,9 +1,7 @@
-import enum
-
 TURBO_STREAM_MIME_TYPE = "text/vnd.turbo-stream.html"
 
 
-class ResponseFormat(enum.Enum):
-    HTML = "html"
-    JSON = "json"
-    TurboStream = "turbo_stream"
+class ResponseFormat:
+    html = False
+    json = False
+    turbo_stream = False
