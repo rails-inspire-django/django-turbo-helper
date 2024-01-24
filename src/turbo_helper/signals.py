39a8f4e44e73ad3ef4e76875049e7859437bfe18
Michaelyin <michaelyin@accordbox.com>
2024-01-24 15:27:41 +0800

    fire after transaction commit

--- src/turbo_helper/signals.py
+++ src/turbo_helper/signals.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+from django.db import transaction
 from django.db.models.signals import post_delete, post_save
 
 
@@ -5,8 +8,10 @@ def after_create_commit(sender):
     def decorator(handler_func):
         def wrapper(sender, instance, created, **kwargs):
             if created:
-                # Call the original signal handler function
-                handler_func(sender, instance, created=created, **kwargs)
+                transaction.on_commit(
+                    # Call the original signal handler function
+                    partial(handler_func, instance, created=created, **kwargs)
+                )
 
         # Connect the wrapper function to the post_save signal
         post_save.connect(wrapper, sender=sender)
@@ -22,8 +27,10 @@ def after_update_commit(sender):
     def decorator(handler_func):
         def wrapper(sender, instance, created, **kwargs):
             if not created:
-                # Call the original signal handler function
-                handler_func(sender, instance, created=created, **kwargs)
+                transaction.on_commit(
+                    # Call the original signal handler function
+                    partial(handler_func, instance, created=created, **kwargs)
+                )
 
         # Connect the wrapper function to the post_save signal
         post_save.connect(wrapper, sender=sender)
@@ -38,8 +45,10 @@ def after_update_commit(sender):
 def after_delete_commit(sender):
     def decorator(handler_func):
         def wrapper(sender, instance, **kwargs):
-            # Call the original signal handler function
-            handler_func(sender, instance, **kwargs)
+            transaction.on_commit(
+                # Call the original signal handler function
+                partial(handler_func, instance, **kwargs)
+            )
 
         # Connect the wrapper function to the post_delete signal
         post_delete.connect(wrapper, sender=sender)
